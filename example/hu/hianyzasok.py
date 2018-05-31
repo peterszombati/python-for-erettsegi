c7610cc30c542ef903d666d4023adb8e36feb320
Peter Szombati <szombatipeti7@gmail.com>
2018-05-31 22:51:45 +0200

    .

--- example/hu/hianyzasok.py
+++ example/hu/hianyzasok.py
@@ -4,132 +4,96 @@ def feladat(x):
 # 1. feladat
 f = open('forras/naplo.txt', 'r')
 content = f.read()
+data = []
 f.close()
 content = content.split('\n')
+datum = [-1,-1]
 for i in range(len(content)):
 	content[i] = content[i].strip("\r")
-
+	if (len(content[i]) > 0):
+		content[i] = content[i].split(' ')
+		if(content[i][0][0] == '#'):
+			datum = [int(content[i][1]),int(content[i][2])]
+		elif(datum[0] != -1):
+			nev = ''
+			for i2 in range(len(content[i]) - 1):
+				if (i2 != 0):
+					nev += ' '
+				nev += content[i][i2]
+			data.append([nev, content[i][len(content[i]) - 1], datum[0], datum[1]])
+def a(x):
+	return x[0]
+data = sorted(data, key=a)
 # 2. feladat
-feladat(2)
-
-hianyzas = 0
-for i in range(len(content)):
-	if (content[i][0] != "#" and len(content[i]) > 0):
-		hianyzas += 1
-print('A naplóban ',hianyzas,' bejegyzés van.',sep='')
+print('2. feladat')
+print('A naplóban ',len(data),' bejegyzés van.',sep='')
 
 # 3. feladat
-'''
-O = jelen
-X = igazolt
-I = igazolatlan
-'''
-feladat(3)
-
-i = 0
-x = 0
-for j in range(len(content)):
-	if (len(content[j]) > 0 and content[j][0] != "#"):
-		line = content[j].split(' ')
-		line = line[len(line) - 1]
-		for j2 in range(len(line)):
-			if (line[j2] == 'X'):
-				x += 1
-			elif (line[j2] == 'I'):
-				i += 1
-print('Az igazolt hiányzások száma ',x,', az igazolatlanoké ',i,' óra.',sep='')
+print('3. feladat')
+x=0
+y=0
+for i in data:
+	i = i[1]
+	for j in i:
+		if(j == 'X'):
+			x+=1
+		elif(j == 'I'):
+			y+=1
+print('Az igazolt hiányzások száma ',x,', az igazolatlanoké ',y,' óra.',sep='')
 
 # 4. feladat
 def hetnapja(honap, nap):
-	napnev = ["vasarnap", "hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat"]
+	napnev = ["vasarnap","hetfo","kedd","szerda","csutortok","pentek","szombat"]
 	napszam = [0,31,59,90,120,151,181,212,243,273,304,335]
 	napsorszam = (napszam[honap - 1] + nap) % 7
 	return napnev[napsorszam]
 
 # 5. feladat
-feladat(5)
-
+print('5. feladat')
 honap = int(input('A hónap sorszáma='))
 nap = int(input('A nap sorszáma='))
-print('Azon a napon ',hetnapja(honap, nap),' volt.',sep='')
+print('Azon a napon ',hetnapja(honap,nap),' volt.',sep='')
 
 # 6. feladat
-feladat(6)
-
-napneve = input('A nap neve=')
+print('6. feladat')
+nap = input('A nap neve=')
 ora = int(input('Az óra sorszáma='))
-aktiv = False
-hianyzas = 0
-for i in range(len(content)):
-	if (len(content[i]) > 0):
-		if (content[i][0] == "#"):
-			line = content[i].split(' ')
-			nap = int(line[2])
-			honap = int(line[1])
-			if (hetnapja(honap, nap) == napneve):
-				aktiv = True
-			else:
-				aktiv = False
-		elif (aktiv == True):
-			line = content[i].split(' ')
-			line = line[len(line) - 1]
-			if (len(line) >= ora and (line[ora - 1] =='I' or line[ora - 1] == 'X')):
-				hianyzas += 1
-print('Ekkor összesen ',hianyzas,' óra hiányzás történt.',sep='')
-
+h = 0
+for i in data:
+	if(hetnapja(i[2],i[3]) == nap):
+		if (len(i[1]) >= ora):
+			if (i[1][ora - 1] == 'X' or i[1][ora - 1] == 'I'):
+				h += 1
+print('Ekkor összesen ',h,' óra hiányzás történt.',sep='')
 # 7. feladat
-feladat(7)
-
+print('7. feladat')
+for i in range(len(data)):
+	h = 0
+	for j in data[i][1]:
+		if (j == 'X' or j == 'I'):
+			h+=1
+	data[i][1] = h
 tanulok = []
-
-def tanulo(data):
+for i in range(len(data)):
 	place = -1
-	for i in range(len(tanulok)):
-		if (len(tanulok[i]) == len(data)):
-			match = True
-			for i2 in range(len(tanulok[i]) - 1):
-				if (tanulok[i][i2] != data[i2]):
-					match = False
-					break
-			if (match == True):
-				place = i
-				break
-
-	line = data[len(data) - 1]
+	for i2 in range(len(tanulok)):
+		if (data[i][0] == tanulok[i2][0]):
+			place = i2
+			break
 	if (place == -1):
-		tanulok.append(data)
-		place = len(tanulok) - 1
-		tanulok[place][len(tanulok[place]) - 1] = 0
+		tanulok.append([data[i][0],data[i][1]])
+	else:
+		tanulok[place][1] += data[i][1]
+def b(x):
+	return x[1]
+tanulok = sorted(tanulok, key=b)
+legtobb = tanulok[len(tanulok) - 1][1]
 
-	hianyzas = 0
-	for i in line:
-		if (i == 'X' or i == 'I'):
-			hianyzas += 1
-	tanulok[place][len(tanulok[place]) - 1] += hianyzas
-
-for i in range(len(content)):
-	if (len(content[i]) > 0 and content[i][0] != "#"):
-		tanulo(content[i].split(' '))
-
-def a(key):
-	return key[len(key) - 1]
-
-tanulok = sorted(tanulok, key=a)
-
-legtobb = tanulok[len(tanulok) - 1][len(tanulok[len(tanulok) - 1]) - 1]
-
-nevek = ''
 i = len(tanulok) - 1
-while(i >= 0):
-	d = tanulok[i]
+nevek = ''
+while(i >= 0 and tanulok[i][1] == legtobb):
+	nevek += ' '
+	nevek += tanulok[i][0]
 	i -= 1
-	if (d[len(d) -1] != legtobb):
-		break
+print('A legtöbbet hiányzó tanulók:',nevek,sep='')
 
-	i2 = 0
-	while(i2 < len(d) - 1):
-		if (len(nevek) > 0):
-			nevek += ' '
-		nevek += d[i2]
-		i2 += 1
-print('A legtöbbet hiányzó tanulók: ',nevek,sep='')
